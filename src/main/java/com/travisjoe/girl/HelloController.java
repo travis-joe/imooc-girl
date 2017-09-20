fd6523b4b4f932853bdda3a4dec8ed9f906004dd
wb-qcy313750 <wb-qcy313750@antfin.com>
2017-09-20 17:52:14 +0800

    数据库

--- src/main/java/com/travisjoe/girl/HelloController.java
+++ src/main/java/com/travisjoe/girl/HelloController.java
@@ -1,6 +1,7 @@
 package com.travisjoe.girl;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
