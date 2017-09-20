af53da0bc40b869ad4be795d0148c1f9c0762df8
qzy <weelgod@gmail.com>
2017-09-20 08:10:29 +0800

    first commit

---
+++ src/main/java/com/travisjoe/girl/HelloController.java
@@ -0,0 +1,20 @@
+package com.travisjoe.girl;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @Value("${cupSize}")
+    private String cupSize;
+
+    @Value("${age}")
+    private int age;
+
+    @RequestMapping(value="/hello", method= RequestMethod.GET)
+    public String say() {
+        return "Hello Spring Boot\n"+cupSize+age;
+    }
+}
